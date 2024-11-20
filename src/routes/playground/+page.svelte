6443b541c8cd0e2ea81a551daedf0852fa2d7727
Cahil <cahil712@gmail.com>
2024-11-20 20:17:49 +0200

    initialize project with Svelte, Tailwind CSS, and Drizzle ORM setup

---
+++ src/routes/playground/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	let name = $state('Hello');
+</script>
+
+<section class="p-24">
+	<h1>{name}</h1>
+	<input bind:value={name} />
+	<button onclick={() => (name = 'World')}>Change name</button>
+</section>
